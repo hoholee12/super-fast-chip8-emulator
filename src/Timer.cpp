b44fd8b8376e34707f4cc06310e94f4055ee1929
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-04-02 01:38:33 +0900

    oops carriage return

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,19 +1,39 @@
 #include"Timer.h"
 
+//for main
 void Timer::init(uint32_t* input){
 	delayTimerPerFrame = input;
-	cycleCount = 0;
+	//cycleCount = 0;
+	temp = 1;
+	secondaryInput = &temp;										//save as 1
+
+	calcLimit();												//calculate new limit
 }
 
-void Timer::updateTimer(uint32_t timerInput){
+//for sub
+void Timer::init(uint32_t* input, uint32_t *secondaryInput){
+	delayTimerPerFrame = input;
+	//cycleCount = 0;
+	this->secondaryInput = secondaryInput;						//save for later
+
+	calcLimit();												//calculate new limit
+}
+
+void Timer::calcLimit(){
+	cycleLimit = (*delayTimerPerFrame / *secondaryInput) - 1;	//pre calculation
+}
+
+
+void Timer::updateTimer(){
+
 	cycleCount++;
-	if (cycleCount > (*delayTimerPerFrame / timerInput) - 1) cycleCount = 0;
-	
+	if (cycleCount > cycleLimit) cycleCount = 0;
 }
 
 bool Timer::checkTimer(){
 
 	if (cycleCount == 0){
+		calcLimit();											//calculate new limit
 		return true;
 	}
 
