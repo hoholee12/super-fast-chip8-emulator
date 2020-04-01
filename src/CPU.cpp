6a95daa55ddc582eb023f270307841dfe368bb3e
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-04-02 00:19:29 +0900

    cleanup

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -98,12 +98,12 @@ void CPU::init(Memory* memory, uint8_t* delayRegister, uint8_t* pressedKey){
 	opcode_table[0xf] = &CPU::opcodetoTablef;
 
 	//table 0
-	for (uint16_t i = 0; i < 0xe0; i++) opcode_table_0[i] = &CPU::opcodenull;		//exception
+	for (uint16_t i = 0; i < OPCODE_TABLE_0_SIZE; i++) opcode_table_0[i] = &CPU::opcodenull;		//exception
 	opcode_table_0[0xe0] = &CPU::opcode00e0;
 	opcode_table_0[0xee] = &CPU::opcode00ee;
-	for (uint16_t i = 0xef; i < OPCODE_TABLE_0_SIZE; i++) opcode_table_0[i] = &CPU::opcodenull;		//exception
 
 	//table 8
+	for (uint16_t i = 0x0; i < OPCODE_TABLE_8_SIZE; i++) opcode_table_8[i] = &CPU::opcodenull;		//exception
 	opcode_table_8[0x0] = &CPU::opcode8xy0;
 	opcode_table_8[0x1] = &CPU::opcode8xy1;
 	opcode_table_8[0x2] = &CPU::opcode8xy2;
@@ -112,37 +112,24 @@ void CPU::init(Memory* memory, uint8_t* delayRegister, uint8_t* pressedKey){
 	opcode_table_8[0x5] = &CPU::opcode8xy5;
 	opcode_table_8[0x6] = &CPU::opcode8xy6;
 	opcode_table_8[0x7] = &CPU::opcode8xy7;
-	for (uint16_t i = 0x8; i < 0xe; i++) opcode_table_8[i] = &CPU::opcodenull;	//exception
 	opcode_table_8[0xe] = &CPU::opcode8xye;
-	for (uint16_t i = 0xf; i < OPCODE_TABLE_8_SIZE; i++) opcode_table_8[i] = &CPU::opcodenull;		//exception
-	
-	//table e
-	for (uint16_t i = 0x0; i < 0x9e; i++) opcode_table_e[i] = &CPU::opcodenull;	//exception
-	opcode_table_e[0x9e] = &CPU::opcodeex9e;
-	for (uint16_t i = 0x9f; i < 0xa1; i++) opcode_table_e[i] = &CPU::opcodenull;	//exception
-	opcode_table_e[0xa1] = &CPU::opcodeexa1;
-	for (uint16_t i = 0xa2; i < OPCODE_TABLE_E_SIZE; i++) opcode_table_e[i] = &CPU::opcodenull;		//exception
 
+	//table e
+	for (uint16_t i = 0x0; i < OPCODE_TABLE_E_SIZE; i++) opcode_table_e[i] = &CPU::opcodenull;		//exception
+	opcode_table_e[0x9e] = &CPU::opcodeex9e;
+	opcode_table_e[0xa1] = &CPU::opcodeexa1;
+	
 	//table f
-	for (uint16_t i = 0x0; i < 0x07; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
+	for (uint16_t i = 0x0; i < OPCODE_TABLE_F_SIZE; i++) opcode_table_f[i] = &CPU::opcodenull;		//exception
 	opcode_table_f[0x07] = &CPU::opcodefx07;
-	for (uint16_t i = 0x08; i < 0x0a; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x0a] = &CPU::opcodefx0a;
-	for (uint16_t i = 0x0b; i < 0x15; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x15] = &CPU::opcodefx15;
-	for (uint16_t i = 0x16; i < 0x18; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x18] = &CPU::opcodefx18;
-	for (uint16_t i = 0x19; i < 0x1e; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x1e] = &CPU::opcodefx1e;
-	for (uint16_t i = 0x1f; i < 0x29; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x29] = &CPU::opcodefx29;
-	for (uint16_t i = 0x2a; i < 0x33; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x33] = &CPU::opcodefx33;
-	for (uint16_t i = 0x34; i < 0x55; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x55] = &CPU::opcodefx55;
-	for (uint16_t i = 0x56; i < 0x65; i++) opcode_table_f[i] = &CPU::opcodenull;	//exception
 	opcode_table_f[0x65] = &CPU::opcodefx65;
-	for (uint16_t i = 0x66; i < OPCODE_TABLE_F_SIZE; i++) opcode_table_f[i] = &CPU::opcodenull;		//exception
 
 	
 	srand(time(NULL));	//random seed
