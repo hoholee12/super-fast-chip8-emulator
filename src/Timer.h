717f879173b4f1c6e4682ada7149ed46f79e040d
hoholee12 <hoholee12@naver.com>
2020-04-02 01:21:33 +0900

    finally fixed the goddamn unresponsive timer that never seemed to be fixable until now :D

--- src/Timer.h
+++ src/Timer.h
@@ -6,11 +6,19 @@
 class Timer{
 private:
 	uint32_t *delayTimerPerFrame; //delay timer
-	uint32_t cycleCount;
+	uint32_t *secondaryInput;
+
+	uint32_t cycleCount = 0;
+	uint32_t cycleLimit;
+
+	uint32_t temp;
+
+	void calcLimit();
 
 public:
-	void init(uint32_t* input);
-	void updateTimer(uint32_t timerInput = 1);
+	void init(uint32_t* input);	//for main
+	void init(uint32_t* input, uint32_t* secondaryInput);	//for sub
+	void updateTimer();
 
 	//return true if reached an end of delay
 	bool checkTimer();
