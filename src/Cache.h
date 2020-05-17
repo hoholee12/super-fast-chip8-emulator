b0b474e9811a5261b0fd7c14738f610a8c493a86
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-05-18 02:12:18 +0900

    Update Cache.h

--- src/Cache.h
+++ src/Cache.h
@@ -1,13 +1,45 @@
 #pragma once
-#include<windows.h>
+#include<iostream>
+#include<vector>
+#include<cstdint>
+#include<string>
 
 /*
 	Dyanarec the executable cache club
 
 */
 
-class Cache{
+using vect8 = std::vector<uint8_t>;
+using string = std::string;
 
+class Cache{
+public:
+#define CACHE_SIZE 4096
+	vect8* iCache[CACHE_SIZE];
+	string dumpfile;
+
+	Cache(string dumpfile){
+		this->dumpfile = dumpfile;
+		//init with NULL
+		for (int i = 0; i < CACHE_SIZE; i++) iCache[i] = NULL;
+	}
+
+	bool checkCacheExists(int pc){
+		if (iCache[pc] != NULL) return true;
+		else return false;
+	}
+
+	vect8* createCache(int pc){
+		if (iCache[pc] != NULL) delete iCache[pc];
+		iCache[pc] = new vect8();
+		return iCache[pc];
+	}
+
+	void dumpCache(int pc){
+		if (iCache[pc] == NULL) return;
+		printf("pc = %02X cache dump:\n", pc);
+		for (int i = 0; i < iCache[pc]->size(); i++) printf("%02X ", iCache[pc]->at(i));
+	}
 
 
 };
